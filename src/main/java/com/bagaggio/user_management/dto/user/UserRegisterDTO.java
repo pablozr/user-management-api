1e4801f177be0741fdeedb82c8bd40566fb22764
Pablo <apollo.zero@outlook.com>
2025-09-10 16:30:42 -0300

    fixing miss information

--- src/main/java/com/bagaggio/user_management/dto/user/UserRegisterDTO.java
+++ src/main/java/com/bagaggio/user_management/dto/user/UserRegisterDTO.java
@@ -28,7 +28,7 @@ public class UserRegisterDTO {
     @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$", message = "A senha deve ter letras minusculas, maiusculas e numeros.")
     private String password;
 
-    @Size(max = 250, message = "A bio deve ter no maximo 250 caracteres.")
+    @Size(max = 500, message = "A bio deve ter no maximo 250 caracteres.")
     private String bio;
 
     @Size(max = 100, message = "O nome completo deve ter no maximo 100 caracteres.")
